f927a5394df96591c7b0fb20b9e0e3bb93a7edd4
gyu0714 <rlaaalsrb3472@gmail.com>
2022-05-31 11:54:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+<<<<<<< Updated upstream
 import RootRoute from "./Routes";
 
 function App() {
   return <RootRoute />;
+=======
+import "./App.css";
+import HeaderLayout from "./Components/HeaderLayout";
+
+function App() {
+  return <HeaderLayout />;
+>>>>>>> Stashed changes
 }
 
 export default App;

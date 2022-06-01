5a5deff36c83763a6c1463652ae17401304182e2
hyeonchaeshin <scsoks@gmail.com>
2022-06-01 16:56:52 +0900

    add axios, setupProxy.js
    작업 초안이라서 작업 App.js에서 진행,
    setupProxy의 경우 무조건 src아래에 있어야 한다고 함.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-import RootRoute from "./Routes";
-
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return <RootRoute />;
+  useEffect(() => {
+    const SERVICE_KEY =
+      '5zDIIIvDyVrv6kqL13%2FHyo1HHfozlGEHg4BogpO6oC2e9oySRLTCQfUAVzsY7RKUJD1qP1t4iNvE9FTviGHwHA%3D%3D';
+    const url = `/getFestivalKr?serviceKey=${SERVICE_KEY}&pageNo=1&numOfRows=10&resultType=json`;
+    axios
+      .get('/api' + url)
+      .then((res) => console.log(res.data.getFestivalKr.item));
+  }, []);
+  return <div></div>;
 }
 
 export default App;

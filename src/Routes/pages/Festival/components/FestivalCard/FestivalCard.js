0d2879fae11781a9d99c6fbf294b04307a121eeb
Gyu0714 <88519797+gyu95@users.noreply.github.com>
2022-06-02 04:00:45 +0900

    [UPDATE] HOME slide

--- src/Routes/pages/Festival/components/FestivalCard/FestivalCard.js
+++ src/Routes/pages/Festival/components/FestivalCard/FestivalCard.js
@@ -51,7 +51,7 @@ const FestivalCard = ({ festivals }) => {
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                {festival.SUBTITLE}
+                {festival.TITLE}
               </Typography>
               <Typography variant="body2" color="text.secondary">
                 {festival.ITEMCNTNTS}
--- src/Routes/pages/Home/HomePresenter.js
+++ src/Routes/pages/Home/HomePresenter.js
@@ -1,86 +1,12 @@
-// import React from 'react';
-// import { AutoSlide } from './Components';
+import React from 'react';
+import AutoSlide from './Components/AutoSlide/AutoSlide';
 
-// const HomePresenter = () => {
-//   return <></>;
-// };
-
-// export default HomePresenter;
-
-import * as React from 'react';
-// import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { Paper, Button } from '@mui/material';
-
-import Carousel from 'react-material-ui-carousel';
-
-export default function HomePresenter() {
-  var items = [
-    {
-      name: 'Random Name #1',
-      description: 'Probably the most random thing you have ever seen!',
-    },
-    {
-      name: 'Random Name #2',
-      description: 'Hello World!',
-    },
-  ];
-
+const HomePresenter = () => {
   return (
-    <Grid sx={{ flexGrow: 1 }} container spacing={0}>
-      <Grid>
-        <Grid sx={{ display: 'flex' }}>
-          <Paper
-            sx={{
-              height: '50vh',
-              width: '27.5vw',
-            }}
-          >
-            <Carousel>
-              {items.map((item, i) => (
-                <Item key={i} item={item} />
-              ))}
-            </Carousel>
-          </Paper>
-          <Paper
-            sx={{
-              height: '50vh',
-              width: '27.5vw',
-            }}
-          ></Paper>
-          <Paper
-            sx={{
-              height: '50vh',
-              width: '27.5vw',
-            }}
-          ></Paper>
-        </Grid>
-        <Grid style={{ display: 'flex' }}>
-          {[0, 1].map((value) => (
-            <Grid key={value} item>
-              <Paper
-                sx={{
-                  height: '50vh',
-                  width: '41vw',
-                  backgroundColor: (theme) =>
-                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                }}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
+    <>
+      <AutoSlide />
+    </>
   );
-}
+};
 
-function Item(props) {
-  return (
-    <Paper>
-      <h2>{props.item.name}</h2>
-      <p>{props.item.description}</p>
-
-      <Button className="CheckButton">Check it out!</Button>
-    </Paper>
-  );
-}
+export default HomePresenter;

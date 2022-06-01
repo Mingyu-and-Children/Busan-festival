9c8fb2152ddc8c976057ca86f9f3c14620d70d50
UIJIN <62687865+kki45@users.noreply.github.com>
2022-06-01 23:00:34 +0900

    괜차나

--- src/api/MapContainer.js
+++ src/api/MapContainer.js
@@ -18,15 +18,15 @@ const MapContainer = () => {
     //   level: 14, // 지도의 확대 레벨
     // });
 
-    // // 마커 클러스터러를 생성합니다
+    // // 마커 클러스터러를 생성
     const clusterer = new kakao.maps.MarkerClusterer({
       map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
       averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-      minLevel: 5, // 클러스터 할 최소 지도 레벨
+      minLevel: 3, // 클러스터 할 최소 지도 레벨
     });
 
     // 마커들을 저장할 변수 생성(마커 클러스터러 관련)
-    var markers = [];
+    const markers = [];
 
     const data = {
       positions: [
@@ -57,9 +57,9 @@ const MapContainer = () => {
       ],
     };
 
-    for (var i = 0; i < data.positions.length; i++) {
-      // 지도에 마커를 생성하고 표시한다.
-      var marker = new kakao.maps.Marker({
+    for (const i = 0; i < data.positions.length; i++) {
+      // 지도에 마커를 생성하고 표시
+      const marker = new kakao.maps.Marker({
         position: new kakao.maps.LatLng(
           data.positions[i].lat,
           data.positions[i].lng,
